fb37a3ab07684ccde2c55ba71103789dc84cc726
Min-creador38 <marinaguerramoreno398@gmail.com>
2024-08-27 10:40:26 -0600

    3SEM_ESTRUCTURA DE DATOS1

--- 03-listas.js
+++ 03-listas.js
@@ -1,17 +1,33 @@
 // crear un lista con diferentes tipos de datos
 let datos ={nombre: "Mina", aprlliods: " Guerra Moreno", edad: 18, estudia: false};
-let arreglo= ["mina", 1, mari, true];
-/*
-const info=()=>{
-    for(let arr in arreglo){
+let arreglo= ["mina", 18, mari, true];
+
+const info = () => {
+    /*for(let arr in arreglo){
         console.log(arreglo[arr]);
 
     }
-}
-info();*/
-
-
-for (let list in datos){
+        for (let list in datos){
     console.log(`${list}:${datos[list]}`)
-}
+    
+    let valores = Object.values(datos);
+    valores.map((data, index) =>{
+        console.log(`${index}:${data}`);
+    });
+
+    let valores = Object.keys(datos);
+    for (let informacion of valores ){
+        console.log(informacion);
+    }
+
+    let valores = Object.entries(data);
+    valores.map(dat => {
+        console.log(dat);
+    })*/
+
+
+        Object.entries(datos).forEach(([key, value]) =>{
+            console.log(`El ${key}: ${value}`)
+        })
+};
 info();
