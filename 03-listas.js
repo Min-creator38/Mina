af0fe3ba51fde5f3869c619cf54956396eb95960
Min-creador38 <marinaguerramoreno398@gmail.com>
2024-08-27 09:50:57 -0600

    Marina

---
+++ 03-listas.js
@@ -0,0 +1,17 @@
+// crear un lista con diferentes tipos de datos
+let datos ={nombre: "Mina", aprlliods: " Guerra Moreno", edad: 18, estudia: false};
+let arreglo= ["mina", 1, mari, true];
+/*
+const info=()=>{
+    for(let arr in arreglo){
+        console.log(arreglo[arr]);
+
+    }
+}
+info();*/
+
+
+for (let list in datos){
+    console.log(`${list}:${datos[list]}`)
+}
+info();
