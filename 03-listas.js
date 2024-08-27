9a0d1c96d851a35ad08d64f008b64e5bf9c7d5d8
Min-creador38 <marinaguerramoreno398@gmail.com>
2024-08-27 11:01:34 -0600

    Trabajando con listas

--- 03-listas.js
+++ 03-listas.js
@@ -1,6 +1,16 @@
 // crear un lista con diferentes tipos de datos
-let datos ={nombre: "Mina", aprlliods: " Guerra Moreno", edad: 18, estudia: false};
-let arreglo= ["mina", 18, mari, true];
+let datos =[{
+    nombre: "Gerardo", 
+    apellido: " Guerra Moreno", 
+    edad: 32, 
+    estudia: false
+},
+{
+    nombre: "Marina",
+    apellido: "Guerra",
+    edad: 18,
+    estudiante: true
+}];
 
 const info = () => {
     /*for(let arr in arreglo){
@@ -23,11 +33,16 @@ const info = () => {
     let valores = Object.entries(data);
     valores.map(dat => {
         console.log(dat);
-    })*/
+    })
 
 
         Object.entries(datos).forEach(([key, value]) =>{
             console.log(`El ${key}: ${value}`)
+        });*/
+
+        
+        datos.map((data) =>{
+            console.log(`El nombre es : ${data.nombre} ${data.apellido} y tiene ${data.edad}`)
         })
 };
 info();
